56ca7a208a20901229f80420349b973a717f60c3
trung87x <job.dinhquangtrung@gmail.com>
2025-10-01 15:49:22 +0700

    .
    .

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class", // ✅ bật dark theo class
-  content: ["./index.html", "./src/**/*.{js,ts,html}"],
-  theme: { extend: {} },
-  plugins: [],
-};
